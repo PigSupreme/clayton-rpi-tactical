c4a3dd4f4e0b9764e773553fff35f3eaf5bb5636
Michael J. Dancs <MichaelDancs@clayton.edu>
2016-04-10 15:23:38 -0400

    Minor reorganizatoin prior to adding group behaviours. Moved demo code from vehichle2d.py to new file demo_stalking.py

---
+++ vehicle/demo_stalking.py
@@ -0,0 +1,177 @@
+#!/usr/bin/env python
+"""Non-flocking vehicle demo."""
+
+# for python3 compat
+from __future__ import unicode_literals
+from __future__ import absolute_import
+from __future__ import print_function
+
+import os, sys, pygame
+from pygame.locals import RLEACCEL, QUIT, MOUSEBUTTONDOWN
+from random import randint, shuffle
+
+INF = float('inf')
+
+# Note: Adjust this depending on where this file ends up.
+sys.path.insert(0, '../vpoints')
+from point2d import Point2d
+
+def load_image(name, colorkey=None):
+    """Loads image from current working directory for use in pygame.
+
+    Parameters
+    ----------
+    name: string
+        Image file to load (must be pygame-compatible format)
+    colorkey: pygame.Color
+        Used to set a background color for this image that will be ignored
+        during blitting. If set to -1, the upper-left pixel color will be
+        used as the background color. See pygame.Surface.set_colorkey() for
+        further details.
+
+    Returns
+    -------
+    (pygame.Surface, pygame.rect):
+        For performance reasons, the returned Surface is the same format as
+        the pygame display. The alpha channel is removed.
+
+    """
+    imagefile = os.path.join(os.getcwd(), name)
+    try:
+        image_surf = pygame.image.load(imagefile)
+    except pygame.error, message:
+        print('Error: Cannot load image: %s' % name)
+        raise SystemExit(message)
+
+    # This converts the surface for maximum blitting performance,
+    # including removal of any alpha channel:
+    image_surf = image_surf.convert()
+
+    # This sets the background (ignored during blit) color:
+    if colorkey is not None:
+        if colorkey is -1:
+            colorkey = image_surf.get_at((0,0))
+        image_surf.set_colorkey(colorkey, RLEACCEL)
+    return image_surf, image_surf.get_rect()
+
+
+if __name__ == "__main__":
+    from vehicle2d import PointMass2d, StaticMass2d, SimpleWall2d
+
+
+    pygame.init()
+
+    # Display constants
+    size = sc_width, sc_height = 1080, 960
+    screen = pygame.display.set_mode(size)
+    bgcolor = 111, 145, 192
+
+    # Sprite images and pygame rectangles
+    numveh = 5
+    numobs = 25
+
+    img = list(range(numveh+numobs))
+    rec = list(range(numveh+numobs))
+    img[0], rec[0] = load_image('rpig.png', -1)
+    for i in range(1, 2):
+        img[i], rec[i] = load_image('ypig.png', -1)
+    for i in range(2, numveh):
+        img[i], rec[i] = load_image('gpig.png', -1)
+
+    # Static obstacles
+    for i in range(numveh, numveh + numobs):
+        img[i], rec[i] = load_image('circle.png', -1)
+
+    StaticMass2d.tagged_image = load_image('circle_tag.png', -1)[0]
+
+    # Object physics for vehicles
+    pos = [Point2d(randint(30, sc_width-30), randint(30, sc_height-30)) for i in range(numveh)]
+    pos[0] = Point2d(sc_width/2, sc_height/2)
+    vel = Point2d(20,0)
+
+    # Array of vehicles for pygame
+    obj = [PointMass2d(img[i], rec[i], pos[i], 50, vel) for i in range(numveh)]
+
+    # Static obstacles for pygame
+    yoffset = sc_height/(numobs+1)
+    yvals = list(range(yoffset, sc_height-yoffset, yoffset))
+    shuffle(yvals)
+    for i in range(numveh, numveh + numobs):
+        offset = (i+1.0-numveh)/(numobs+1)
+        rany = yvals[i-numveh]
+        pos.append(Point2d(offset*sc_width, rany))
+        obj.append(StaticMass2d(img[i], rec[i], pos[i], 10, vel))
+    # This gives a convenient list of obstacles for later use
+    obslist = obj[numveh:]
+
+    # Static Walls for pygame: TESTING
+    wall_list = (SimpleWall2d((sc_width//2, 10), sc_width-20, 4, Point2d(0,1)),
+                 SimpleWall2d((sc_width//2, sc_height-10), sc_width-20, 4, Point2d(0,-1)),
+                 SimpleWall2d((10, sc_height//2), sc_height-20, 4, Point2d(1,0)),
+                 SimpleWall2d((sc_width-10,sc_height//2), sc_height-20, 4, Point2d(-1,0)))
+#                 SimpleWall2d((2+sc_width//2, 2+sc_height//2), min(sc_width,sc_height), 4, Point2d(1,1)),
+#                 SimpleWall2d((sc_width//2, sc_height//2), min(sc_width,sc_height), 4, Point2d(-1,-1)),
+#                 SimpleWall2d((sc_width//3, sc_height//2), sc_height//3, 4, Point2d(1,0)),
+#                 SimpleWall2d((sc_width//3-2, sc_height//2), sc_height//3, 4, Point2d(-1,0))
+#                 }
+
+    obj.extend(wall_list)
+
+
+
+    # Set-up pygame rendering
+    allsprites = pygame.sprite.RenderPlain(obj)
+
+
+    ### Vehicle behavior defined below ###
+
+    # Big red arrow: Wander and Avoid obstacles
+    obj[0].maxspeed = 4.0
+    obj[0].steering.set_target(WANDER=(250, 50, 10))
+    obj[0].radius = 100
+
+    #Old examples, updated for new syntax
+    #obj[1].steering.set_target(SEEK=(500,300))
+    #obj[1].steering.set_target(FLEE=(500,400))
+
+    # Yellow arrow: Guard RED from GREEN leader
+    obj[1].maxspeed = 5.0
+    obj[1].steering.set_target(GUARD=(obj[0], obj[2], 0.2))
+
+    # Green arrow leader; TAKECOVER from YELLOW
+    obj[2].maxspeed = 3.0
+    obj[2].steering.set_target(PURSUE=obj[0], TAKECOVER=(obj[1], obslist, 200, True))
+    # This was old demo:
+    #obj[2].steering.set_target(PURSUE=obj[0], EVADE=obj[1])
+
+    # Green arrow followers: Follow GREEN leader and evade YELLOW
+    obj[3].maxspeed = 3.0
+    obj[3].steering.set_target(FOLLOW=(obj[2], Point2d(-20,20)), EVADE=obj[1])
+    obj[4].maxspeed = 3.0
+    obj[4].steering.set_target(FOLLOW=(obj[2], Point2d(-20,-20)), EVADE=obj[1])
+
+    # All vehicles will avoid obstacles and walls
+    for i in range(numveh):
+        obj[i].steering.set_target(AVOID=obslist, WALLAVOID=[50, wall_list])
+
+    ### End of vehicle behavior ###
+
+    while 1:
+        for event in pygame.event.get():
+            if event.type in [QUIT, MOUSEBUTTONDOWN]:
+                pygame.quit()
+                sys.exit()
+
+        for wall in wall_list:
+            wall.tagged = False
+
+        allsprites.update(0.4)
+
+        # pygame.time.delay(2)
+
+        # Render
+        screen.fill(bgcolor)
+        allsprites.draw(screen)
+        pygame.display.flip()
+
+    pygame.time.delay(2000)
--- vehicle/vehicle2d.py
+++ vehicle/vehicle2d.py
@@ -7,8 +7,7 @@ from __future__ import absolute_import
 from __future__ import print_function
 
 import os, sys, pygame
-from pygame.locals import RLEACCEL, QUIT, MOUSEBUTTONDOWN
-from random import randint, shuffle
+from pygame.locals import RLEACCEL
 
 INF = float('inf')
 
@@ -49,6 +48,11 @@ def load_image(name, colorkey=None):
         For performance reasons, the returned Surface is the same format as
         the pygame display. The alpha channel is removed.
 
+    Note
+    ----
+    TODO: This function isn't really needed here; but doesn't yet have a
+    permanent home. Fix this later.
+
     """
     imagefile = os.path.join(os.getcwd(), name)
     try:
@@ -70,10 +74,10 @@ def load_image(name, colorkey=None):
 
 class SimpleWall2d(pygame.sprite.Sprite):
     """A simple wall for use in Pygame.
-    
+
     Parameters
     ----------
-    
+
     center: tuple or Point2d
         The center of the wall in screen coordinates.
     length: int
@@ -83,15 +87,15 @@ class SimpleWall2d(pygame.sprite.Sprite):
     f_normal: Point2d
         Normal vector out from the front of the wall.
     color: 3-tuple or pygame.Color, optional
-        Color for rendering. Defaults to (0,0,0)    
+        Color for rendering. Defaults to (0,0,0)
     """
-    
+
     def __init__(self, center, length, thick, f_normal, color=None):
         # Must call pygame's Sprite.__init__ first!
         pygame.sprite.Sprite.__init__(self)
-        
+
         # Set-up original image for rendering
-        self.orig = pygame.Surface((length,thick))            
+        self.orig = pygame.Surface((length, thick))
         self.orig.set_colorkey((255,0,255))
         if color == None:
             self.color = (0,0,0)
@@ -105,15 +109,16 @@ class SimpleWall2d(pygame.sprite.Sprite):
         self.left = self.front.left_normal()
         self.rsq = (length/2)**2
         #print("Wall normal = %s, Wall left = %s" % (self.front, self.left))
-        
+
         # Put into place for rendering
         self.image = pygame.transform.rotate(self.orig, self.theta)
         self.rect = self.image.get_rect()
         self.rect.center = center[0], center[1]
         self.center = center
         self.tagged = False
-        
+
     def update(self, delta_t=1.0):
+        """Update for use by pygame.Sprite parent class."""
         if self.tagged is True:
             color = (255,255,255)
         else:
@@ -127,7 +132,7 @@ class SimpleWall2d(pygame.sprite.Sprite):
         self.image = pygame.transform.rotate(self.orig, self.theta)
         self.rect = self.image.get_rect()
         self.rect.center = self.center[0], self.center[1]
-            
+
 
 class PointMass2d(pygame.sprite.Sprite):
     """A pygame.Sprite with rectilnear motion.
@@ -255,7 +260,7 @@ class PointMass2d(pygame.sprite.Sprite):
         self._rotate_for_blit()
 
         # Simple edge warping
-        self.pos = Point2d(self.pos[0] % sc_width, self.pos[1] % sc_height)
+        #self.pos = Point2d(self.pos[0] % sc_width, self.pos[1] % sc_height)
 
 class StaticMass2d(PointMass2d):
     """A stationary pygame.Sprite. Use for fixed obstacles.
@@ -326,119 +331,4 @@ class RotatingMass2d(PointMass2d):
         self.rotate(delta_t)
 
 if __name__ == "__main__":
-    pygame.init()
-
-    # Display constants
-    size = sc_width, sc_height = 1080, 960
-    screen = pygame.display.set_mode(size)
-    bgcolor = 111, 145, 192
-
-    # Sprite images and pygame rectangles
-    numveh = 5
-    numobs = 25
-
-    img = list(range(numveh+numobs))
-    rec = list(range(numveh+numobs))
-    img[0], rec[0] = load_image('rpig.png', -1)
-    for i in range(1, 2):
-        img[i], rec[i] = load_image('ypig.png', -1)
-    for i in range(2, numveh):
-        img[i], rec[i] = load_image('gpig.png', -1)
-
-    # Static obstacles
-    for i in range(numveh, numveh + numobs):
-        img[i], rec[i] = load_image('circle.png', -1)
-
-    StaticMass2d.tagged_image = load_image('circle_tag.png', -1)[0]
-
-    # Object physics for vehicles
-    pos = [Point2d(randint(30, sc_width-30), randint(30, sc_height-30)) for i in range(numveh)]
-    pos[0] = Point2d(sc_width/2, sc_height/2)
-    vel = Point2d(20,0)
-
-    # Array of vehicles for pygame
-    obj = [PointMass2d(img[i], rec[i], pos[i], 50, vel) for i in range(numveh)]
-
-    # Static obstacles for pygame
-    yoffset = sc_height/(numobs+1)
-    yvals = list(range(yoffset, sc_height-yoffset, yoffset))
-    shuffle(yvals)
-    for i in range(numveh, numveh + numobs):
-        offset = (i+1.0-numveh)/(numobs+1)
-        rany = yvals[i-numveh]
-        pos.append(Point2d(offset*sc_width, rany))
-        obj.append(StaticMass2d(img[i], rec[i], pos[i], 10, vel))
-    # This gives a convenient list of obstacles for later use
-    obslist = obj[numveh:]
-
-    # Static Walls for pygame: TESTING
-    wall_list = (SimpleWall2d((sc_width//2, 10), sc_width-20, 4, Point2d(0,1)),
-                 SimpleWall2d((sc_width//2, sc_height-10), sc_width-20, 4, Point2d(0,-1)),
-                 SimpleWall2d((10, sc_height//2), sc_height-20, 4, Point2d(1,0)),
-                 SimpleWall2d((sc_width-10,sc_height//2), sc_height-20, 4, Point2d(-1,0)))
-#                 SimpleWall2d((2+sc_width//2, 2+sc_height//2), min(sc_width,sc_height), 4, Point2d(1,1)),
-#                 SimpleWall2d((sc_width//2, sc_height//2), min(sc_width,sc_height), 4, Point2d(-1,-1)),
-#                 SimpleWall2d((sc_width//3, sc_height//2), sc_height//3, 4, Point2d(1,0)),
-#                 SimpleWall2d((sc_width//3-2, sc_height//2), sc_height//3, 4, Point2d(-1,0))
-#                 }
-    
-    obj.extend(wall_list)
-        
-    
-
-    # Set-up pygame rendering
-    allsprites = pygame.sprite.RenderPlain(obj)
-
-
-    ### Vehicle behavior defined below ###
-
-    # Big red arrow: Wander and Avoid obstacles
-    obj[0].maxspeed = 4.0
-    obj[0].steering.set_target(WANDER=(250, 50, 10))
-    obj[0].radius = 100
-
-    #Old examples, updated for new syntax
-    #obj[1].steering.set_target(SEEK=(500,300))
-    #obj[1].steering.set_target(FLEE=(500,400))
-
-    # Yellow arrow: Guard RED from GREEN leader
-    obj[1].maxspeed = 5.0
-    obj[1].steering.set_target(GUARD=(obj[0], obj[2], 0.2))
-
-    # Green arrow leader; TAKECOVER from YELLOW
-    obj[2].maxspeed = 3.0
-    obj[2].steering.set_target(PURSUE=obj[0], TAKECOVER=(obj[1], obslist, 200, True))   
-    # This was old demo:    
-    #obj[2].steering.set_target(PURSUE=obj[0], EVADE=obj[1]) 
-
-    # Green arrow followers: Follow GREEN leader and evade YELLOW
-    obj[3].maxspeed = 3.0
-    obj[3].steering.set_target(FOLLOW=(obj[2], Point2d(-20,20)), EVADE=obj[1])
-    obj[4].maxspeed = 3.0
-    obj[4].steering.set_target(FOLLOW=(obj[2], Point2d(-20,-20)), EVADE=obj[1])
-
-    # All vehicles will avoid obstacles and walls
-    for i in range(numveh):
-        obj[i].steering.set_target(AVOID=obslist, WALLAVOID=[50, wall_list])
-
-    ### End of vehicle behavior ###
-
-    while 1:
-        for event in pygame.event.get():
-            if event.type in [QUIT, MOUSEBUTTONDOWN]:
-                pygame.quit()
-                sys.exit()
-        
-        for wall in wall_list:
-            wall.tagged = False
-
-        allsprites.update(0.4)
-
-        # pygame.time.delay(2)
-
-        # Render
-        screen.fill(bgcolor)
-        allsprites.draw(screen)
-        pygame.display.flip()
-
-    pygame.time.delay(2000)
+    print("Two-Dimensional Vehicle/Obstacle Functions. Import this elsewhere")
