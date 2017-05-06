e762029d26f40e8c0fed8e7679ccf022bcaf4834
Michael J. Dancs <MichaelDancs@clayton.edu>
2017-05-06 17:52:39 -0400

    Initial clean-up to hydro_fish.py; work in progress.

--- vehicle/hydro_fish.py
+++ vehicle/hydro_fish.py
@@ -10,157 +10,41 @@ from __future__ import division
 import sys, pygame
 from pygame.locals import RLEACCEL, QUIT, MOUSEBUTTONDOWN
 
-POINTMASS2D_MASS = 5
-POINTMASS2D_MAXSPEED = 80
-POINTMASS2D_MAXFORCE = 50000
+# TODO: Adjust this depending on where this file ends up.
+sys.path.extend(['../vpoints', '../vehicle'])
+from point2d import Point2d
+
+# BasePointMass2d defaults
+import vehicle2d
+vehicle2d.POINTMASS2D_MASS = 5
+vehicle2d.POINTMASS2D_MAXSPEED = 80
+vehicle2d.POINTMASS2D_MAXFORCE = 50000
+
+# Math defaults
+from math import sqrt
+INF = float('inf')
+
+# Fish coefficients (coefishents??)
 DAMPING_COEFF = 10
 SPRING_CONST = 3
 MUSCLE_K = 90
 SOLIDS_K = 100
 CROSS_K = 150
 TAIL_K = 80
-FISH = True
 FREQ = 240
 SQUEEZE = 0.88
 HYDRO_FORCE_SCALE = 0.02
-
 UPDATE_SPEED = 0.02
 
-from math import sqrt
-INF = float('inf')
 
-# TODO: Adjust this depending on where this file ends up.
-sys.path.extend(['../vpoints', '../vehicle'])
-from point2d import Point2d
-
-# Point2d functions return radians, but pygame wants degrees. The negative
-# is needed since y coordinates increase downwards on screen. Multiply a
-# math radians result by SCREEN_DEG to get pygame screen-appropriate degrees.
-SCREEN_DEG = -57.2957795131
-
-# A PointMass has its heading aligned with velocity. However, if the speed is
-# almost zero (squared speed is below this threshold), we skip alignment in
-# order to avoid jittery behaviour.
-SPEED_EPSILON = .000000001
-
-class PointMass2dSprite(pygame.sprite.Sprite):
-    """A Pygame sprite used to display a BasePointMass2d object."""
-
-    def __init__(self, owner, img_surf, img_rect):
-        # Must call pygame's Sprite.__init__ first!
-        pygame.sprite.Sprite.__init__(self)
-
-        self.owner = owner
-
-        # Pygame image information for blitting
-        self.orig = img_surf
-        self.image = img_surf
-        self.rect = img_rect
-
-    def update(self, delta_t=1.0):
-        """Called by pygame.Group.update() to redraw this sprite."""
-        # owner = self.owner
-        # Update position
-        self.rect.center = self.owner.pos[0], self.owner.pos[1]
-        # Rotate for blitting
-#        theta = owner.front.angle()*SCREEN_DEG
-#        center = self.rect.center
-#        self.image = pygame.transform.rotate(self.orig, theta)
-#        self.rect = self.image.get_rect()
-#        self.rect.center = center
-
-class BasePointMass2d(object):
-    """A moving object with rectilinear motion and optional sprite.
-
-    Parameters
-    ----------
-    position: Point2d
-        Center of mass, in screen coordinates.
-    radius: float
-        Bounding radius of the object.
-    velocity: Point2d
-        Velocity vector, in screen coordinates. Initial facing matches this.
-    spritedata: list or tuple, optional
-        Extra data used to create an associate sprite. See notes below.
-
-    Notes
-    -----
-    This provides a minimal base class for a pointmass with bounding radius
-    and heading aligned to velocity. Use move() for physics updates each
-    cycle (including applying force).
-
-    As we typically will be rendering these objects within some environment,
-    the constructor provides an optional spritedata parameter that can be used
-    to create an associated sprite. This is currently implemented using the
-    PointMass2dSprite class above (derived from pygame.sprite.Sprite), but
-    can be overridden by changing the _spriteclass attribute.
-    """
-    _spriteclass = PointMass2dSprite
-    """Default sprite class to use for rendering."""
-
-    def __init__(self, position, radius, velocity, spritedata=None):
-        # Basic object physics
-        # Note: We can't use self.pos = position here because of Point2d's
-        # __init__ method (and lack of __copy__), ditto for self.vel.
-        self.pos = Point2d(position[0], position[1])  # Center of object
-        self.radius = radius                          # Bounding radius
-        self.vel = Point2d(velocity[0], velocity[1])  # Current Velocity
-
-        # Normalized front vector in world coordinates.
-        # This stays aligned with the object's velocity (using move() below)
-#        try:
-#            self.front = velocity.unit()
-#        except ZeroDivisionError:
-#            # If velocity is <0,0>, set facing to screen upwards
-#            self.front = Point2d(0,-1)
-#        self.left = Point2d(-self.front[1], self.front[0])
-
-        # Movement constraints (defaults from top of file)
-        self.mass = POINTMASS2D_MASS
-        self.maxspeed = POINTMASS2D_MAXSPEED
-        self.maxforce = POINTMASS2D_MAXFORCE
-
-        if spritedata is not None:
-            self.sprite = PointMass2dSprite(self, *spritedata)
-
-    def move(self, delta_t=1.0, force_vector=None):
-        """Updates position, velocity, and acceleration.
-
-        Parameters
-        ----------
-        delta_t: float
-            Time increment since last move.
-        force_vector: Point2d, optional
-            Constant force during for this update.
-        """
-        # Update position using current velocity
-        self.pos = self.pos + self.vel.scale(delta_t)
-
-        # Apply force, if any...
-        if force_vector:
-            # Don't exceed our maximum force; compute acceleration/velocity
-            force_vector.truncate(self.maxforce)
-            accel = force_vector.scale(delta_t/self.mass)
-            self.vel = self.vel + accel
-        # ..but don't exceed our maximum speed
-        self.vel.truncate(self.maxspeed)
-
-        # Align heading to match our forward velocity. Note that
-        # if velocity is very small, skip this to avoid jittering.
-        if self.vel.sqnorm() > SPEED_EPSILON:
-            self.front = self.vel.unit()
-            self.left = Point2d(-self.front[1], self.front[0])
-
-
-class SpringMass2d(BasePointMass2d):
+class SpringMass2d(vehicle2d.BasePointMass2d):
     """A point mass that can accumulate several forces and apply all at once.
     
     TODO: Rename this to something more general.
     """
-    
     def __init__(self, position, mass, velocity, spritedata=None):
         radius = 5
-        BasePointMass2d.__init__(self, position, radius, velocity, spritedata)
+        vehicle2d.BasePointMass2d.__init__(self, position, radius, velocity, spritedata)
         self.mass = mass
         self.accumulated_force = Point2d(0,0)
         
@@ -197,7 +81,6 @@ class IdealSpring2d(object):
     (from mass1 to mass2). This orientation is used internally, but has no
     visible effect outside of the exert_force update.
     """
-
     def __init__(self, spring_constant, rest_length, mass1, mass2):
         self.k = spring_constant
         # Give negative rest_length to use current distance between masses
@@ -242,7 +125,6 @@ class MuscleSpring2d(IdealSpring2d):
     mathematically by changing the natural length of the underlying spring; see
     the contract() method for further details.
     """
-    
     def __init__(self, spring_constant, mass1, mass2, contraction_factor):
         IdealSpring2d.__init__(self, spring_constant, -1, mass1, mass2)
         self.flexlength = self.natlength
@@ -474,7 +356,6 @@ if __name__ == "__main__":
         # Render
         screen.fill(bgcolor)
         
-
         # Manually render each spring
         for spring in springs:
             scale = spring.curscale
@@ -494,15 +375,14 @@ if __name__ == "__main__":
                 tipvec = quad.pos - quad.current_force
                 tip = [int(x) for x in tipvec.ntuple()]
                 pygame.draw.line(screen, (0,90,190), center, tip, 2)
-                
-        # 
-
+            
         # Render regular sprites (point masses)
         allsprites.draw(screen)            
         pygame.display.flip()
         ticks = ticks + 1
 
-        #pygame.time.delay(2)
-
     # Clean-up here
     pygame.time.delay(2000)
+    pygame.quit()
+    sys.exit()
+    
